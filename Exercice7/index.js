841dfbc87119994b3591f465de7d9d6d9df97abb
aladin002dz <mahfoudh.arous@gmail.com>
2024-07-04 09:53:02 +0100

    code initial

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -1,24 +1,19 @@
 function convertToBinary() {
-    let decimalInput = document.getElementById("decimalInput").value;
-    let binaryResult = document.getElementById("binaryResult");
-    binaryResult.innerHTML = "";
-
-    if (decimalInput === "") {
-        binaryResult.innerHTML = "Veuillez entrer un nombre décimal";
-        return;
+    const decimalInput = document.getElementById("decimalInput").value;
+    if (isNaN(decimalInput)) {
+      alert("Veuillez entrer un nombre décimal valide.");
+      return;
     }
-
-    let binaryString = "";
-    let decimalNumber = parseInt(decimalInput);
-    console.log(decimalNumber);
-    if (isNaN(decimalNumber)) {
-        binaryResult.innerHTML = "Veuillez entrer un nombre décimal valide";
-        return;
+  
+    let binaryResult = "";
+    let decimalValue = parseInt(decimalInput);
+  
+    while (decimalValue > 0) {
+      const remainder = decimalValue % 2;
+      binaryResult = remainder + binaryResult;
+      decimalValue = Math.floor(decimalValue / 2);
     }
-    while (decimalNumber > 0) {
-        binaryString += decimalNumber % 2;
-        decimalNumber = Math.floor(decimalNumber / 2);
-    }
-    binaryString = binaryString.split("").reverse().join("");
-    binaryResult.innerHTML = binaryString;
-}
+  
+    document.getElementById("binaryResult").textContent = binaryResult;
+  }
+  
